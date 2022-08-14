de9977a174542b37013066ad5edb8f57175bad0b
DevMasteryLabs <develop.devmastery@gmail.com>
2022-08-14 11:08:10 +0100

    Link navbar items with pages

--- src/App.js
+++ src/App.js
@@ -15,8 +15,6 @@ function App() {
         <Route path='/projects' component={Projects} />
         <Route path='/skills' component={Skills} />
         <Route path='/contact' component={Contact} />
-        <h1>Hello</h1>
-        <button className="btn btn-success">OK</button>
       </BrowserRouter>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,26 +1,27 @@
 import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <Link to='/' className="navbar-brand" >Ahmed Zribi</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <NavLink exact to='/' className="nav-link">Home</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Projects</a>
+              <NavLink to='/projects' className="nav-link">Projects</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Skills</a>
+              <NavLink to='/skills' className="nav-link">Skills</NavLink>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
+              <NavLink to='/contact' className="nav-link">Contact</NavLink>
             </li>
           </ul>
         </div>
