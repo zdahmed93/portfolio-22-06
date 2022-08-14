a0e2e5b1bea001c1a081c851d762f95b3a05401f
DevMasteryLabs <develop.devmastery@gmail.com>
2022-08-14 12:50:21 +0100

    Add content to the home page

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
+    <BrowserRouter>
+      <Navbar />
+      <div className='container mt-5'>
         <Route exact path='/' component={Home} />
         <Route path='/projects' component={Projects} />
         <Route path='/skills' component={Skills} />
         <Route path='/contact' component={Contact} />
-      </BrowserRouter>
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import devImage from '../images/photo-2.png'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <img src="/images/photo-1.jpg" alt="Profile photo" width="200px" />
+      <p>Hello my name is Ahmed. I am a Web Developer.</p>
+      <img src={devImage} alt="Web Image" width="40%" />
+    </div>
   )
 }
 
