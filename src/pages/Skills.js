cd8ddfacee3692a52c3673581d9a413e983b8f88
DevMasteryLabs <develop.devmastery@gmail.com>
2022-08-14 13:34:51 +0100

    Display skills

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Skills() {
+  const [skills, setSkills] = useState([])
+  useEffect(() => {
+    axios.get('https://devmastery-assets.vercel.app/technologies.json')
+      .then((response) => { setSkills(response.data) })
+      .catch((error) => { console.log(error) })
+  }, [])
   return (
-    <div>Skills</div>
+    <div className='row' >
+      {skills.map(item => (
+        <div key={item.id} className='col-12 col-sm-6 col-md-4 col-lg-3 p-3' >
+          <div class="card">
+            <img src={item.image} class="card-img-top" alt={`${item.name} Logo`} />
+              <div class="card-body">
+                <h5 class="card-title"> {item.name} </h5>
+                {/* <p class="card-text"> {item.description} </p> */}
+                <a href="#" class="btn btn-primary">Learn about</a>
+              </div>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
