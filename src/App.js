68981545f5debaf593de12953a0b0e0a6e2d521d
DevMasteryLabs <develop.devmastery@gmail.com>
2022-08-14 10:26:07 +0100

    Add pages and set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
+import Contact from './pages/Contact'
+import Projects from './pages/Projects'
+import Skills from './pages/Skills'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <h1>Hello</h1>
-      <button className="btn btn-success">OK</button>
+      <BrowserRouter>
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/skills' component={Skills} />
+        <Route path='/contact' component={Contact} />
+        <h1>Hello</h1>
+        <button className="btn btn-success">OK</button>
+      </BrowserRouter>
     </div>
   );
 }
