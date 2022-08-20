850b94abb5245286cad443d5a98e496edff4f3d9
DevMasteryLabs <develop.devmastery@gmail.com>
2022-08-20 12:32:13 +0100

    Add a paragraph in the Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,8 @@ function Home() {
   return (
     <div>
       <img src="/images/photo-1.jpg" alt="Profile photo" width="200px" />
-      <p>Hello my name is Ahmed. I am a Web Developer.</p>
+      <p>Hello my name is Ahmed.</p>
+      <p>I am a Web Developer.</p>
       <img src={devImage} alt="Web Image" width="40%" />
     </div>
   )
