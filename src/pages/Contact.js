d54b93f19e1bfcc50113aea38a4e50e56aa0cde8
DevMasteryLabs <develop.devmastery@gmail.com>
2022-08-20 10:03:35 +0100

    Add content to the Contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,22 @@
 import React from 'react'
 
 function Contact() {
+  const contactInformations = {
+    email: 'ahmed@devmastery.tech',
+    phoneNumber: '+216 50 122 128',
+    address: {
+      city: 'Sfax',
+      country: 'Tunisia',
+      street: 'ROUTE AFRANE KM 2',
+      postalCode: 3089
+    }
+  }
   return (
-    <div>Contact</div>
+    <div>
+      <p> <i class="bi bi-envelope"></i> {contactInformations.email} </p>
+      <p> <i class="bi bi-telephone"></i> {contactInformations.phoneNumber} </p>
+      <p> <i class="bi bi-geo-alt"></i> {contactInformations.address.street}; {contactInformations.address.postalCode} {contactInformations.address.city}; {contactInformations.address.country} </p>
+    </div>
   )
 }
 
